6ac515587f264ebbacefc7acf63c469e7342210a
Reece Laudat <45715991+ReeceLaudat@users.noreply.github.com>
2025-01-26 11:52:58 +0000

    Now returns data and changed Facebook page action order
    - Added so the functions return data (url for ebay and page source for Fbmp)
    - Changed the order that selenium goes through the Fbmp due to the Scraper.py not being able to grab the right data

--- search.py
+++ search.py
@@ -1,10 +1,8 @@
-from bs4 import element
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 class findGPU:
@@ -19,8 +17,8 @@ class findGPU:
         sleep(3)
         elementBIN = driver.find_element(By.XPATH, "/html/body/div[5]/div[4]/div[1]/div/div[2]/div[2]/div[1]/div/ul/li[3]/a")
         driver.get(elementBIN.get_attribute("href"))
+        return driver.current_url
 
-        
     def facebook_search(self, gpu):
         driver = webdriver.Firefox()
         driver.get("https://www.facebook.com/marketplace")
@@ -30,13 +28,7 @@ class findGPU:
         elementCloseLogin = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/div/i") 
         elementCloseLogin.click()
 
-        elementSearchBar = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div/div/div/span/div/div/div/div/label/input")
-
-        elementSearchBar.send_keys(gpu)
-        elementSearchBar.send_keys(Keys.ENTER)
-        sleep(3)
-
-        elementLocationFilter = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div/span")
+        elementLocationFilter = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div/div/div[4]/div[2]/div[1]/div[1]/div/span")
         elementLocationFilter.click()
         sleep(1)
         elementSetLocation = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[3]/div/div[1]/div[2]/div/div/div/div/div/div/label/div[2]/input[1]")
@@ -48,6 +40,15 @@ class findGPU:
         elementSelectLocation.click()
         elementLocationApply = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[4]/div/div[2]/div/div/div/div/div/div/div[1]/div/span/span")
         elementLocationApply.click()
+
+        elementSearchBar = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div/div/div/span/div/div/div/div/label/input")
+
+        elementSearchBar.send_keys(gpu)
+        elementSearchBar.send_keys(Keys.ENTER)
+        sleep(3)
+
+
+        return driver.page_source
 
 
     def search_both(gpu):
